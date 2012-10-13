bb8f76957b2276016c7a19e1637ba5e3a237a4ad
Ken Mazaika <kenmazaika@gmail.com>
2012-10-13 19:02:28 -0400

    remove activerecord cuz' im a baller

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
+
+# Auto-require default libraries and those for the current Rails environment. 
+Bundler.require :default, Rails.env
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -23,7 +31,6 @@ module Dexter
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -45,13 +52,11 @@ module Dexter
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
 
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,3 @@ Dexter::Application.config.session_store :cookie_store, key: '_dexter_session'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Dexter::Application.config.session_store :active_record_store
