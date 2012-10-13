550566661352fdf67053044a1d11de36602e7d89
Ken Mazaika <kenmazaika@gmail.com>
2012-10-13 16:59:16 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dexter::Application.config.secret_token = 'c9174d20be0d36fe593163250acbef3459bc119c9da8bd7b6bab114bf940286fdb935bffc1f03afe5f0f58edb3d99729b4f839aea237e43102e6f0b08f5208b1'
