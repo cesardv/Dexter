2fb8c35d8a5973a5e165c27375ca37ae77ff4fe3
Ken Mazaika <kenmazaika@gmail.com>
2012-10-13 20:11:32 -0400

    Allow any url to match an id, even allow slashes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Dexter::Application.routes.draw do
   resources :drops
+  match '/drops/:id', :constraints => {:id => /.*/}, :controller => :drops, :action => :show
+
+
   root :to => 'drops#new'
 end
